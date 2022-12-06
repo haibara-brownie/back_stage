0e4ebe998217c3c754c36304939c70990f2581d0
haibarabrownie <haibara@rainai.onmicrosoft.com>
2022-12-06 17:10:29 +0800

    feat: form

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/styles/index.scss'
-import SvgIcon from '@/icons'
+import SvgIcon from '@/icons/index'
 
 const app = createApp(App)
 SvgIcon(app)
